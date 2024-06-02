2be4bdfaf4599e601abcc598718390db1d1a1eab
brioso22 <marcosbriosobenavides2002@gmail.com>
2024-06-02 02:43:34 -0300

    mejora en barra 2 -6 -2024

--- resources/js/components/BarraNav.vue
+++ resources/js/components/BarraNav.vue
@@ -6,18 +6,29 @@
       </div>
       <nav>
         <ul>
-          <li> <a href="#" @click="redirectTo(index)">Inicio </a></li>
-          <li> <a href="#" @click="redirectTo(tienda)">Tienda</a> </li>
-          <li><a href="#" @click="redirectTo(preguntasF)">Preguntas Frecuentes</a></li>
-          <li><a href="#" @click="redirectTo(donaciones)">Donaciones</a></li>
-          <li><a href="#" @click="redirectTo(soporte)">Soporte Técnico</a></li>
+          <li @mouseover="showTooltip('Página de incio', $event)" @mouseout="hideTooltip">
+            <a href="#" @click="redirectTo(index)">Inicio</a>
+          </li>
+          <li @mouseover="showTooltip('Visita nuestra tienda con promociones y productos inigualables', $event)" @mouseout="hideTooltip">
+            <a href="#" @click="redirectTo(tienda)">Tienda</a>
+          </li>
+          <li @mouseover="showTooltip('Consulta tu dudades recurrentes en la seccion de preguntas frecuentes', $event)" @mouseout="hideTooltip">
+            <a href="#" @click="redirectTo(preguntasF)">Preguntas Frecuentes</a>
+          </li>
+          <li @mouseover="showTooltip('¿Tienes harware que ya no necesites? Haz una donación en nuestra seccion especial', $event)" @mouseout="hideTooltip">
+            <a href="#" @click="redirectTo(donaciones)">Donaciones</a>
+          </li>
+          <li @mouseover="showTooltip('Obtén soporte técnico para todos tus equipos', $event)" @mouseout="hideTooltip">
+            <a href="#" @click="redirectTo(soporte)">Soporte Técnico</a>
+          </li>
         </ul>
       </nav>
       <div class="auth-buttons">
-        <button @click="redirectTo(loginUrl)">Iniciar Sesión</button>
-        <button @click="redirectTo(registerUrl)">Registro</button>
+        <button @click="redirectTo(loginUrl)" @mouseover="showTooltip('Inicia sesión', $event)" @mouseout="hideTooltip">Iniciar Sesión</button>
+        <button @click="redirectTo(registerUrl)" @mouseover="showTooltip('Regístrate', $event)" @mouseout="hideTooltip">Registro</button>
       </div>
     </div>
+    <span class="tooltip" v-if="tooltipVisible" :style="{ top: tooltipY + 'px', left: tooltipX + 'px' }">{{ tooltipText }}</span>
   </header>
 </template>
 
@@ -42,36 +53,48 @@ export default {
       required: true,
     },
     preguntasF: {
-      type:String,
-      required:true,
+      type: String,
+      required: true,
     },
-    donaciones:{
-      type:String,
-      required:true,
+    donaciones: {
+      type: String,
+      required: true,
     },
-
-    soporte:{
-      type:String,
-      required:true,
+    soporte: {
+      type: String,
+      required: true,
     },
-    
+  },
+  data() {
+    return {
+      tooltipText: '',
+      tooltipVisible: false,
+      tooltipX: 0,
+      tooltipY: 0,
+    };
   },
   methods: {
     redirectTo(url) {
       window.location.href = url;
     },
-
-
+    showTooltip(text, event) {
+      this.tooltipText = text;
+      this.tooltipVisible = true;
+      this.tooltipX = event.clientX + 10; // offset to avoid mouse cursor overlap
+      this.tooltipY = event.clientY + 10;
+    },
+    hideTooltip() {
+      this.tooltipVisible = false;
+    },
   },
 };
 </script>
 
 <style scoped>
-
 body {
   margin: 0;
   font-family: Arial, sans-serif;
-  background-color: #9032bb; /* Color de fondo agregado */
+  background-color: #9032bb;
   padding: 0;
   height: 150px;
   overflow: hidden;
@@ -81,6 +104,7 @@ header {
   background-color: #333;
   color: #fff;
   padding: 10px 0;
+  position: relative;
 }
 
 .container {
@@ -105,6 +129,7 @@ nav ul {
 
 nav ul li {
   margin: 0 10px;
+  position: relative;
 }
 
 nav ul li a {
@@ -126,12 +151,22 @@ nav ul li a {
 .auth-buttons button:hover {
   background-color: #751e99;
 }
-a:hover,button:hover{
-color:#4CAF50;
-transition: color 1s;
+
+a:hover, button:hover {
+  color: #4CAF50;
+  transition: color 1s;
 }
 
-
-
-
+.tooltip {
+  display: inline-block;
+  position: fixed;
+  background-color: #333;
+  color: #fff;
+  padding: 5px 10px;
+  border-radius: 5px;
+  white-space: nowrap;
+  z-index: 1000;
+  font-size: 12px;
+  font-size: 2.5dvb;
+}
 </style>
