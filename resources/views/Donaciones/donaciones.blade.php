f47817c49bec48ed4ee235995ece23a3338a7b2d
brioso22 <marcosbriosobenavides2002@gmail.com>
2024-06-03 03:14:17 -0300

    mejora login codigo

--- resources/views/Donaciones/donaciones.blade.php
+++ resources/views/Donaciones/donaciones.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/js/app.js'])
+    <link href="{{ asset('./resours/index.css') }}" rel="stylesheet">
+
     <title>Document</title>
 </head>
 <body>
